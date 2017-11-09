b2ab61465be04cb87d2f718e9c1b406d044f855f
Varun Upadhyay <varun.u28@gmail.com>
2017-11-09 10:08:05 -0800

    Added CoinChange Algorithm in Dynamic Programming

---
+++ src/main/scala/DynamicProgramming/CoinChange.scala
@@ -0,0 +1,34 @@
+package DynamicProgramming
+
+/**
+  * An implementation of famous dynamic programming Coin Change Problem
+  * Problem Statement: Given an amount and a list of coin change, find number of possible
+  * combinations to get the amount
+  */
+
+
+object CoinChange {
+
+  /**
+    *
+    * @param coins   - a list of integers i.e. change coins
+    * @param money   - the target amount
+    * @return - number of combinations possible to get the amount
+    */
+
+  def coinChange(coins: List[Int], money: Int): Int = {
+    val combinations :Array[Int] = new Array[Int](money+1)
+
+    combinations(0) = 1
+
+    for (coin <- coins) {
+      for (i <- coin to money) {
+        if (i >= coin) {
+          combinations(i) += combinations(i-coin)
+        }
+      }
+    }
+
+    combinations(money)
+  }
+}
