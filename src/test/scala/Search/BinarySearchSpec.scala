62be456cf9413dc47860b0cb23c593a9e9b3ae0e
gaurav_miglani <gaurav.miglani@innovaccer.com>
2018-02-02 23:57:27 +0530

    adds binary search return functionality: insertion index

--- src/test/scala/Search/BinarySearchSpec.scala
+++ src/test/scala/Search/BinarySearchSpec.scala
@@ -9,8 +9,27 @@ class BinarySearchSpec extends FlatSpec {
     assert(BinarySearch.binarySearch(l,5) === 4)
   }
 
-  it should "return -1 if the element is not present in the list" in {
+  it should "return (-(insertion point) - 1) if the element is not present in the list" in {
     val l = List.range(1,5)
-    assert(BinarySearch.binarySearch(l,7) === -1)
+    val m = List(1,3,5,7)
+    assert(BinarySearch.binarySearch(l,7) === -5)
+    assert(BinarySearch.binarySearch(l,0) === -1)
+    assert(BinarySearch.binarySearch(m,2) === -2)
+    assert(BinarySearch.binarySearch(m,6) === -4)
+    assert(BinarySearch.binarySearch(m,0) === -1)
+  }
+
+  "A Binary Search With Range" should "return the index of an element in an array" in {
+    val l = List.range(1,10)
+    assert(BinarySearch.binarySearch(l,2,0,5) === 1)
+    assert(BinarySearch.binarySearch(l,5,3,6) === 4)
+    assert(BinarySearch.binarySearch(l,2,6,9) === -7)
+    assert(BinarySearch.binarySearch(l,5,1,3) === -4)
+  }
+
+  it should "return (-(insertion point) - 1). if the element is not present in the list" in {
+    val l = List.range(1,5)
+    assert(BinarySearch.binarySearch(l,7,0,4) === -5)
+    assert(BinarySearch.binarySearch(l,7,1,3) === -4)
   }
 }
