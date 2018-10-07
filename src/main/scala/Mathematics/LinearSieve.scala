e410ecc7ecb8a39687eef5c398041096baa0c5b0
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2018-10-07 15:02:42 +0300

    Add algorithm for getting sequence of prime numbers

---
+++ src/main/scala/Mathematics/LinearSieve.scala
@@ -0,0 +1,26 @@
+package Mathematics
+
+object LinearSieve {
+  /**
+    * Method returns sequence of prime numbers which all are not greater than n
+    *
+    * @param n
+    * @return
+    */
+  def getPrimeNumbers(n: Int): Seq[Int] = {
+    var primeNumbers = Seq.empty[Int]
+    val lowestPrimeDivisor: Array[Int] = Array.fill(n + 1)(0)
+    for (i <- 2 to n) {
+      if (lowestPrimeDivisor(i) == 0) {
+        lowestPrimeDivisor(i) = i
+        primeNumbers :+= i
+      }
+      var j = 0
+      while (j < primeNumbers.length && primeNumbers(j) <= lowestPrimeDivisor(i) && i * primeNumbers(j) <= n) {
+        lowestPrimeDivisor(i * primeNumbers(j)) = primeNumbers(j)
+        j = j + 1
+      }
+    }
+    primeNumbers
+  }
+}
---
+++ src/test/scala/Mathematics/LinearSieveSpec.scala
@@ -0,0 +1,23 @@
+package Mathematics
+
+import org.scalatest.FlatSpec
+
+class LinearSieveSpec extends FlatSpec {
+  "Linear sieve" should "return all prime numbers for specific n" in {
+    val n = 15
+    assert(LinearSieve.getPrimeNumbers(n) === List(2, 3, 5, 7, 11, 13))
+  }
+
+  "Linear sieve" should "return only prime numbers for specific n" in {
+    val n = 15000
+    LinearSieve.getPrimeNumbers(n).foreach(x => assert(isPrime(x)))
+  }
+
+  def isPrime(n: Int): Boolean = {
+    for (i <- 2 until n) if (n % i == 0) return false
+    true
+  }
+}
+
+
+
