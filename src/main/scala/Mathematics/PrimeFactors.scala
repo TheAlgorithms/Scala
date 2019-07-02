41247fae8180d6effd6491bbcbb0fd9fb0569d93
aditiagarwal34550 <aditiagarwal34550@gmail.com>
2019-07-01 22:56:36 -0700

    add ons in scala/Mathematics directory -  added PrimeFactors.scala

---
+++ src/main/scala/Mathematics/PrimeFactors.scala
@@ -0,0 +1,19 @@
+package Mathematics
+
+object PrimeFactors {
+
+	/**
+	    * Method returns list of prime factors of number n
+	    *
+	    * @param num
+    	    * @return
+    	*/
+
+	def PrimeFactors(number: Long): List[Long] = {
+	    val exists = (2L to math.sqrt(number).toLong).find(number % _ == 0)
+	    exists match {
+      		case Some(factor) => factor :: primeFactors(number/factor)
+      		case None => List(number)
+    		}
+  	}
+}
