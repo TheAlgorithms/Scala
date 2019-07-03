f398864c254fad6a40631048d5df019f6082ea96
Aditi Agarwal <31546143+aditiagarwal34550@users.noreply.github.com>
2019-07-03 23:09:19 +0530

    Update AbsMax.scala

--- src/main/scala/Mathematics/AbsMax.scala
+++ src/main/scala/Mathematics/AbsMax.scala
@@ -3,18 +3,18 @@ package Mathematics
 object AbsMax {
 
 	/**
-	    * Method returns absolute Max Element from the list
+	    * Method returns absolute Maximum Element from the list
 	    *
 	    * @param listOfElements
     	    * @return
     	*/
 
-	def absMax(Elements : List[Int]): Int = {
-	    var length : Int = Elements.length;
-	    var max : Int = Elements(0)
+	def absMax(elements : List[Int]): Int = {
+	    var length : Int = elements.length;
+	    var max : Int = elements(0)
 	    for (i <- 1 to length -  1) {
-		if (Math.abs(max) < Math.abs(Elements(i))) {
-			max = Elements(i);
+		if (Math.abs(max) < Math.abs(elements(i))) {
+			max = elements(i);
 		}
 	    }
 	    return max;
