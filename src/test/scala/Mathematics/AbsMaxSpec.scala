542f7cacee7e0e92391fcbdd826c06d99804a5d3
Aditi Agarwal <31546143+aditiagarwal34550@users.noreply.github.com>
2019-07-04 13:32:00 +0530

    Update AbsMaxSpec.scala

--- src/test/scala/Mathematics/AbsMaxSpec.scala
+++ src/test/scala/Mathematics/AbsMaxSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.FlatSpec
 class AbsMaxSpec extends FlatSpec {
 
   it should "output the correct Integer as a result from the list of elements" in {
-    assert(AbsMax.absMax(-1000, -1, 999, 72, 65, -56, -767) === -1000)
+    assert(AbsMax.absMax(-1000, -1, 999, 72, 65, -56, -767) === 1000)
   }
 
   it should "output the correct Integer as a result from the list of elements" in {
