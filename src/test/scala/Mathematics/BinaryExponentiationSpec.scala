85855b0df839486c27d4f6cabb0a801509913b7b
Aditi Agarwal <31546143+aditiagarwal34550@users.noreply.github.com>
2019-07-04 10:37:16 +0530

    Update BinaryExponentiationSpec.scala

--- src/test/scala/Mathematics/BinaryExponentiationSpec.scala
+++ src/test/scala/Mathematics/BinaryExponentiationSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.FlatSpec
 class BinaryExponentiationSpec extends FlatSpec {
 
   it should "output the correct Integer as a result for base raised to the power " in {
-    assert(BinaryExponentiation.BinaryExponentiation(2, 3) === 8)
+    assert(BinaryExponentiation.binaryExponentiation(2, 3) === 8)
   }
 
 }
