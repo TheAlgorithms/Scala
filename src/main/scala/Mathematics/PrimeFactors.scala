a5723f520b95886d149f47d0fc04a44ff8337b57
Aditi Agarwal <31546143+aditiagarwal34550@users.noreply.github.com>
2019-07-04 10:36:08 +0530

    Update PrimeFactors.scala

--- src/main/scala/Mathematics/PrimeFactors.scala
+++ src/main/scala/Mathematics/PrimeFactors.scala
@@ -12,8 +12,9 @@ object PrimeFactors {
 	def primeFactors(number: Long): List[Long] = {
 	    val exists = (2L to math.sqrt(number).toLong).find(number % _ == 0)
 	    exists match {
+		case None => List(number)
       		case Some(factor) => factor :: primeFactors(number/factor)
-      		case None => List(number)
+      		
     		}
   	}
 }
