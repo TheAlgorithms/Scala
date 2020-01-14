917b4497dce54b0b08c1a314ec2f6678a3f94272
heka1024 <heka1024@gmail.com>
2020-01-14 14:21:07 +0000

    Modify quicksort and add test case

--- src/test/scala/Sort/QuickSortSpec.scala
+++ src/test/scala/Sort/QuickSortSpec.scala
@@ -5,8 +5,13 @@ import org.scalatest.FlatSpec
 class QuickSortSpec extends FlatSpec {
 
   "A Quick Sort" should "return a sorted version of an array passed to it" in {
-    val arr = Array(3,2,7,1,9,0)
-    assert(QuickSort.quickSort(arr) === Array(0,1,2,3,7,9))
+    val list = List(3,2,7,1,9,0)
+    assert(QuickSort.quickSort(list) === List(0,1,2,3,7,9))
+  }
+  
+  "A Quick Sort" should "return empty list when empty list is passed to it" in {
+    val list: List[Int] = List()
+    assert(QuickSort.quickSort(list) === List())
   }
 
 }
