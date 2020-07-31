1a9e3e0877f9e830983cef6414467d6b95f524d7
khanhhuynguyenvu <khanhhuynguyenvu@gmail.com>
2020-07-31 16:35:42 +0700

    fix(travis): travis automation testing

--- src/test/scala/Mathematics/FindMaxSpec.scala
+++ src/test/scala/Mathematics/FindMaxSpec.scala
@@ -4,11 +4,11 @@ import org.scalatest.FlatSpec
 
 class FindMaxSpec extends FlatSpec {
 
-  it should "output the correct Integer as a result from the list of elements" in {
+  "FindMaxSpec 1" should "output the correct Integer as a result from the list of elements" in {
     assert(FindMax.findMax(List(-1000, -1, 999, 72, 65, -56, -767)) === 999)
   }
 
-  it should "output the correct Integer as a result from the list of elements" in {
+  "FindMaxSpec 2" should "output the correct Integer as a result from the list of elements" in {
     assert(FindMax.findMax(List(121, 221, 3, 4112)) === 4112)
   }
 
