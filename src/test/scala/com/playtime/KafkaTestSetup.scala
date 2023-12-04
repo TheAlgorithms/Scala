029a0f1e5012afda605f4f8a45bed85f43e6bbae
itssrahul <48329540+itssrahul@users.noreply.github.com>
2023-12-04 15:27:56 +0530

    Create KafkaTestSetup.scala

---
+++ src/test/scala/com/playtime/KafkaTestSetup.scala
@@ -0,0 +1,14 @@
+package com.supergloo.examples
+
+import java.util.Properties
+
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.StreamsConfig
+
+trait KafkaTestSetup {
+  val config = new Properties()
+  config.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "testing")
+  config.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "testing:1234")
+  config.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
+  config.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
+}
